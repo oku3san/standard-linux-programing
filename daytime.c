72a450891c11c0dd8e7062a795cb0247e302aa74
oku3san <takaya0303@gmail.com>
2023-02-23 23:40:13 +0900

    Update

--- daytime.c
+++ daytime.c
@@ -18,3 +18,34 @@ main(int argc, char *argv[])
   fputs(buf, stdout);
   exit(0);
 }
+
+static int
+open_connection(char *host, char *service)
+{
+  int sock;
+  struct addrinfo hints, *res, *ai;
+  int err;
+
+  memset(&hints, 0, sizeof(struct addrinfo));
+  hints.ai_family = AF_UNSPEC;
+  hints.ap_socktype = SOCK_STREAM;
+  if ((err = getaddrinfo(host, service, &hints, &res)) != 0) {
+    fprintf(stderr, "getaddrinfo(3): %s\n", gai_strerror(err));
+    exit(1);
+  }
+  for (ai = res; ai = ai->ai_next) {
+    sock = socket(ai->ai_family, ai->ai_coktype, ai->ai_protocol);
+    if (sock < 0) {
+      continue;
+    }
+    if (connect(sock, ai->ai_addr, ai->ai_addrlen) < 0) {
+      close(sock);
+      continue;
+    }
+    freeeaddrinfo(res);
+    return sock;
+  }
+  fprintf(stderr, "socket(2)/connect(2) failed");
+  freeaddrinfo(res);
+  exit(1);
+}
